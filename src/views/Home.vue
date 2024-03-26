603a6b1b14926485737b8781f4b1a75c90010de7
iampeterjohn <peterjeche442@gmail.com>
2024-03-25 23:57:51 -1100

    section4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,35 @@
         <Section1 />
         <section2 />
         <Section3 />
+        <Section4 
+            v-slot="nameItems"
+        >
+            <a href="#">
+                <div 
+                    class="section4-home-1"
+                >
+                    <div>
+                        <img 
+                            :src="nameItems.itemImg" 
+                            class="section4-home-img" 
+                            alt=""
+                        >
+                    </div>
+                    <div 
+                        class="p-4 text-center"
+                    >
+                        <div 
+                            class="section4-home-title-name"
+                        >
+                            {{ nameItems.itemName }}
+                        </div>
+                        <div>
+                            {{ nameItems.itemText }} 
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </Section4>
     </div>
 </template>
 
@@ -10,4 +39,5 @@
     import Section1 from '@/views/astrofirstpage/Section1.vue';
     import Section2 from '@/views/astrofirstpage/Section2.vue';
     import Section3 from '@/views/astrofirstpage/Section3.vue';
+    import Section4 from '@/views/astrofirstpage/Section4.vue';
 </script>
---
+++ src/views/astrofirstpage/Section4.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="section4-I">
+        <div class="container section4-II">
+            <slot  
+                v-for="item in items" 
+                :itemName="item.title" 
+                :itemText="item.text" 
+                :itemImg="item.img" 
+                :key="item.id"
+            ></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const items = ref([
+    {
+        title : 'astronaut',
+        text : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis exercitationem facere, inventore quidem vero eaque nam, quibusdam aperiam ducimus quasi obcaecati omnis blanditiis in dolor? Cumque voluptatibus quibusdam aliquid velit.',
+        img : 'src/assets/img/astronaut-1.png',
+        id : 1
+    },
+    {
+        title : 'outer space',
+        text : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis exercitationem facere, inventore quidem vero eaque nam, quibusdam aperiam ducimus quasi obcaecati omnis blanditiis in dolor? Cumque voluptatibus quibusdam aliquid velit.',
+        img : 'src/assets/img/outer-space.png',
+        id : 2
+    },
+    {
+        title : 'stars',
+        text : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis exercitationem facere, inventore quidem vero eaque nam, quibusdam aperiam ducimus quasi obcaecati omnis blanditiis in dolor? Cumque voluptatibus quibusdam aliquid velit.',
+        img : 'src/assets/img/stars.png',
+        id : 3
+    }
+  ])
+</script>
