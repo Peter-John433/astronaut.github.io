892f346cf267465f9894cc96a945abfcaf160993
iampeterjohn <peterjeche442@gmail.com>
2024-03-25 14:59:30 -1100

    sectionI and section2

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Section1 />
+        <section2 />
+    </div>
+</template>
+
+<script setup>
+    import Section1 from '@/views/astrofirstpage/Section1.vue';
+    import Section2 from '@/views/astrofirstpage/Section2.vue';
+</script>
---
+++ src/views/astrofirstpage/Section1.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="section-I">
+        <div class="section-II container">
+            <div>
+                <img src="@/assets/img/astro-man.png" class="block mx-auto" alt="">
+            </div>
+            <div>
+                <div class="text-headings">
+                    <div>
+                        Exploring
+                    </div>
+                    <div>
+                        Space, inspiring 
+                    </div>
+                    <div>
+                        minds
+                    </div>
+                </div>
+                <div class="astro-text">
+                    unlock the mystries of the cosmos,explorations beyond earth's boundaries
+                </div>
+                <div>
+                    <a href="#" class="section-I-a">
+                         start exploring
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
